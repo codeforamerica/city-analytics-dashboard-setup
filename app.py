60f1e4f7ffbfbb48c0402b8f340f4adde1b37be3
Michal Migurski <mike@teczno.com>
2014-06-07 12:27:14 -0700

    Retrieving web properties list from Google Analytics

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from uuid import uuid4
 import json
 
 from flask import Flask, request, session, redirect, render_template, jsonify
-from requests import post
+from requests import get, post
+import oauth2
 
 google_authorize_url = 'https://accounts.google.com/o/oauth2/auth'
 google_access_token_url = 'https://accounts.google.com/o/oauth2/token'
@@ -28,7 +29,8 @@ def authorize_google():
     session['state'] = state
     
     query_string = urlencode(dict(client_id=client_id, redirect_uri=redirect_uri,
-                                  scope='profile', state=state, response_type='code',
+                                  scope='https://www.googleapis.com/auth/analytics https://www.googleapis.com/auth/analytics.readonly',
+                                  state=state, response_type='code',
                                   access_type='offline', approval_prompt='force'))
     
     return redirect(google_authorize_url + '?' + query_string)
@@ -67,6 +69,33 @@ def callback_google(client_id, client_secret, code, state, redirect_uri):
     access = json.loads(resp.content)
     access_token, token_type = access['access_token'], access['token_type']
     refresh_token = access['refresh_token']
+    
+    url = 'https://www.googleapis.com/analytics/v3/management/accounts/~all/webproperties'
+    
+    #
+    # '{"error":{"errors":[{"domain":"usageLimits","reason":"accessNotConfigured","message":"Access Not Configured. Please use Google Developers Console to activate the API for your project."}],"code":403,"message":"Access Not Configured. Please use Google Developers Console to activate the API for your project."}}'
+    # https://code.google.com/apis/console/ > APIs & Auth > Analytics API "On"
+    #
+    
+    response = get(url, params={'access_token': access_token})
+    
+    properties = [
+        (item['defaultProfileId'], item['name'], item['websiteUrl'])
+        for item in json.loads(response.content).get('items')
+        if item.get('defaultProfileId', False)
+        ]
+    
+    properties.sort(key=lambda p: p[1].lower())
+    
+    return jsonify(dict(poop=properties))
+
+    return render_template('index.html', properties=properties)
+
+    consumer = oauth2.Consumer(client_id, client_secret)
+    token = oauth2.Token(refresh_token, '')
+    client = oauth2.Client(consumer, token)
+    
+    return repr(client.request(url, 'GET'))
     
     return jsonify(dict(client_id=client_id, client_secret=client_secret,
                         access_token=access_token, token_type=token_type,
